5d5a9f6a4219cde010ddda52928e826831b9e28a
Oluwafemi <oluwafemiadedeji143@gmail.com>
2024-09-05 04:36:30 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Footer from "./component/Footer/Footer";
+import Hero from "./component/Hero/Hero";
+import Join from "./component/Join/Join";
+import Plans from "./component/Plans/Plans";
+import Program from "./component/Program/Program";
+import Reasons from "./component/Reasons/Reasons";
+import Testimonial from "./component/Testimonials/Testimonial";
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <Program />
+      <Reasons />
+      <Plans />
+      <Testimonial />
+      <Join />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Plans/Plans.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Plan.css";
+import { plansData } from "../../data/plansData";
+import whiteTick from "../../assets/whiteTick.png";
+
+const Plans = () => {
+  return (
+    <div className="plans-container" id="plans">
+      <div className="blur plan-blur-1"></div>
+      <div className="blur plan-blur-2"></div>
+      <div className="programs-header" style={{ gap: "2rem" }}>
+        <span className="stroke-text">READY TO START</span>
+        <span>YOUR JOURNEY</span>
+        <span className="stroke-text">NOW WITH US</span>
+      </div>
+
+      <div className="plans">
+        {plansData.map((plan, i) => {
+          return (
+            <div className="plan" key={i}>
+              {plan.icon}
+              <span>{plan.name}</span>
+              <span>$ {plan.price}</span>
+
+              <div className="features">
+                {plan.features.map((feature, i) => {
+                  return (
+                    <div className="feature">
+                      <img src={whiteTick} alt="" />
+                      <span key={i}>{feature}</span>
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div>
+                <span>See more benefit </span>
+              </div>
+              <button className="btn">Join now</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Plans;
